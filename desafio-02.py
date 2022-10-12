b452d4a4e530d3de533d2bd20fb39683c69150e2
Marcos Rigel <marcosrigel99@gmail.com>
2022-10-12 14:15:37 -0300

    Aula 04 - primeiros comandos em python3

---
+++ desafio-02.py
@@ -0,0 +1,6 @@
+# Crie um script python que leia o dia, o mês e o ano de nascimento de uma pessoa
+# e mostre uma mensagem com a data formatada
+dia = input("Dia = ")
+mes = input("Mes = ")
+ano = input("Ano = ")
+print('Voce nasceu no dia '+ dia + ' de '+mes+' de ' + ano + '. Correto?')
---
+++ desafio-03.py
@@ -0,0 +1,5 @@
+# Crie um script python que leia dois numeros e tente mostrar a soma entre eles.
+numero01 = input('Primeiro número ')
+numero02 = input('Segundo número ')
+sum(numero01, numero02)
+print("A soma dos numeros é: ", sum(numero01, numero02))
